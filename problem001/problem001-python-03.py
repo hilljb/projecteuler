1350f6f54bd67866b3974f3b5007bfe751061e09
hilljb <jason@jasonbhill.com>
2014-01-03 00:37:58 -0700

    added a set method

--- problem001/problem001-python-03.py
+++ problem001/problem001-python-03.py
@@ -2,25 +2,13 @@
 
 import time
 
-
-# The sum of all numbers from 1 to n is (n*(n+1))/2. Using this formula, we can
-# find the answer in the following way.
-
-# Notice that all of the multiples of 3 look like this:
-#   3 + 6 + 9 + ... + 996 + 999 = 3 * (1 + 2 + ... + 333) = 3 * (333*334)/2
-
-# Similarly, the sum of all multiples of 5 looks like this:
-#   5 + 10 + 15 + ... + 995 = 5 * (1 + 2 + ... + 199) = 5 * (199*200)/2
-
-# If we were to add these two integers together, we would be counting multiples
-# of 15 twice (once as multiples of 3 and once as multiples of 5). So, we take
-# the two numbers above and subtract an instance of all multiples of 15:
-#   15 + 30 + ... + 990 = 15 * (1 + 2 + ... + 66) = 15 * (66*67)/2
-
 start = time.time()
 
-s = ((3*333*334)/2) + ((5*199*200)/2) - ((15*66*67)/2)
-    
+s1 = set(range(0,1000,3))
+s2 = set(range(0,1000,5))
+ss = s1.union(s2)
+s = sum(ss)
+
 elapsed = time.time() - start
 
-print "result %s found in %s seconds" % (s, elapsed)
+print "result %s returned in %s seconds" % (s,elapsed)
