ee7ad4314ad10d9eba41e8d6edefc8ad7a8dc464
hilljb <jason@jasonbhill.com>
2014-03-23 11:55:53 -0600

    added 2nd Python solution

---
+++ problem014/problem14-02.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python2
+
+import time
+ 
+start = time.time()
+ 
+limit = 1000000
+collatz_length = [0] * limit
+collatz_length[1] = 1
+max_length = [1,1]
+ 
+for i in range(1,1000000):
+    n,s = i,0
+    TO_ADD = [] # collatz_length not yet known
+    while n > limit - 1 or collatz_length[n] < 1:
+        TO_ADD.append(n)
+        if n % 2 == 0: n = n/2
+        else: n = 3*n + 1
+        s += 1
+    # collatz_length now known from previous calculations
+    p = collatz_length[n]
+    for j in range(s):
+        m = TO_ADD[j]
+        if m < limit:
+            new_length = collatz_length[n] + s - j
+            collatz_length[m] = new_length
+            if new_length > max_length[1]: max_length = [i,new_length]
+ 
+elapsed = (time.time() - start)
+print "found %s at length %s in %s seconds" % (max_length[0],max_length[1],elapsed)
